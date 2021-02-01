6b6f14642c530a01203df2b085b77e51a7733b2c
colasm <maxime.colas@univ-reims.fr>
2021-02-01 09:52:53 +0100

    premier commit

---
+++ II0905.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic18f46j50mrf24j40 configuration
+CND_ARTIFACT_DIR_pic18f46j50mrf24j40=dist/pic18f46j50mrf24j40/production
+CND_ARTIFACT_NAME_pic18f46j50mrf24j40=II0905.X.production.hex
+CND_ARTIFACT_PATH_pic18f46j50mrf24j40=dist/pic18f46j50mrf24j40/production/II0905.X.production.hex
+CND_PACKAGE_DIR_pic18f46j50mrf24j40=${CND_DISTDIR}/pic18f46j50mrf24j40/package
+CND_PACKAGE_NAME_pic18f46j50mrf24j40=ii0905.x.tar
+CND_PACKAGE_PATH_pic18f46j50mrf24j40=${CND_DISTDIR}/pic18f46j50mrf24j40/package/ii0905.x.tar
